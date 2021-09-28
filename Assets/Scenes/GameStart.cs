ffcdfcff696c1228ebf99af67359a8d57c1ce199
DESKTOP-OHD6DA9\USER <abv021@naver.com>
2021-09-28 09:12:47 +0900

    4

---
+++ Assets/Scenes/GameStart.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class GameStart : MonoBehaviour
+{
+    [SerializeField] private Text txtTitle;
+    private string m_Message;
+    private string m_Message2;
+    private float m_Speed = 0.2f;
+    [SerializeField] private RectTransform Panel;
+    void Start()
+    {
+        m_Message = @" RAISING
+COMPANY
+2";
+
+        StartCoroutine(Typing(txtTitle, m_Message, m_Speed));
+    }
+
+    IEnumerator Typing(Text txtTitle, string message, float speed)
+    {
+        for (int i = 0; i < message.Length; i++)
+        {
+            txtTitle.text = message.Substring(0, i + 1);
+            yield return new WaitForSeconds(speed);
+        }
+        UpDown();
+        yield break;
+    }
+    public void UpDown()
+    {
+        Panel.DOLocalMoveY(100, 1).SetLoops(-1, LoopType.Yoyo);
+    }
+    public void BtnStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
